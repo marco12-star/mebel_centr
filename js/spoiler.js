61ffde3ace8dce2d822185ce5896f56233cd5b84
marco12-star <62327943+marco12-star@users.noreply.github.com>
2020-07-18 20:44:46 +0300

    Moved js to html code and removed spoiler.js

--- js/spoiler.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$('.spoiler__title').click(function(event){
-		if ($('.spoiler').hasClass('one')) {
-			$('.spoiler__title').not($(this)).removeClass('active');
-			$('.spoiler__text').not($(this).next()).slideUp(300)
-		}
-		$(this).toggleClass('active').next().slideToggle(300);
-	});
-});
